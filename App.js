b5310b0028de19d1b16ccb9ebafe754a94c57f17
Dylan <dylanabozarth@gmail.com>
2023-08-02 20:44:16 -0700

    webview method

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return <WebView source={{ uri: 'https://andromedagame.online/' }} style={{ flex: 1 }} />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
+    color: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
